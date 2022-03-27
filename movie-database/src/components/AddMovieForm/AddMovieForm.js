dcdbf6e7aff82578c4c0e456f2e754c2b666bca5
sahrudin <asepsahrudin0399@gmail.com>
2022-03-27 13:46:36 +0700

    Task-Pertemuan-6

---
+++ movie-database/src/components/AddMovieForm/AddMovieForm.js
@@ -0,0 +1,29 @@
+import styles from "./AddMovieForm.module.css";
+
+function AddMovieForm(){
+    return(
+        <div className={styles.container}>
+            <section className={styles.form}>
+
+                <div className={styles.form__left}>
+                <img className={styles.form__image} src="https://picsum.photos/600/400" alt="" />
+                </div>
+
+
+                <div className={styles.form__right}>
+                <h2 className={styles.form__title}>Add Movie</h2>
+                <form className={styles.form__action} action="/handlelogin" method="post">
+                <label className={styles.form__label} for="title">Title</label><br />
+                <input className={styles.form__input} type="text" id="title"  /><br />
+                <label className={styles.form__year} for="text">Year</label><br />
+                <input className={styles.form__input} type="text" id="text" /><br /><br />
+                <input className={styles.form__submit} type="submit" value="Submit" />
+                </form>
+                </div>
+
+            </section>
+        </div>
+    )
+}
+
+export default AddMovieForm;
